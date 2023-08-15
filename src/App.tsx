2546f4b6ae112d3606a8a33f0d06580c4731059d
Joao Junior <223332@IMAC-VIP-001.local>
2023-08-15 10:03:48 -0300

    fix/merge-home-office

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Primeiro from './components/Primeiro'
-import Comp, {Comp1, Comp2 } from './components/Multi'
-// import MinMax from './components/MinMax';
-// import Aleatorio from './components/Aleatorio';
-// import AleatorioTS from './components/AleatorioTS';
-import Titulo from './components/Titulo';
-import Botao from './components/Botao';
-import Contador from './components/Contador2';
+import { View, StyleSheet } from 'react-native';
+// import Primeiro from './components/Primeiro'
+// import Comp, {Comp1, Comp2 } from './components/Multi'
+import MinMax from './components/MinMax';
+import AleatorioTS from './components/AleatorioTS';
+import Pai from './components/direta/Pai';
+import PaiInd from './components/indireta/Pai'
+import PaiIndTS from './components/indireta/PaiTS'
 
 //export default () => <View><Primeiro /></View>
 
 export default () => (
   <View style = {style.App}>
-    {/* <AleatorioTS min={3} max={20}></AleatorioTS> */}
-    {/* <MinMax min={3} max={20} />
-    <MinMax min={8} max={9} /> */}
-    <Titulo principal="Cadastro Produto" secundario="Tela de Cadastro de Produto" />
-    <Contador inicial={100} passo={2}/>
-    {/* <Botao /> */}
+    <AleatorioTS min={3} max={20}></AleatorioTS>
+    <MinMax min={3} max={20} />
+    {/* <MinMax min={8} max={9} /> */}
     {/* <Comp1 /> */}
     {/* <Comp2 /> */}
     {/* <Comp /> */}
     {/* <Primeiro /> */}
+    <Pai />
+    <PaiInd />
+    <PaiIndTS />
+
   </View>
 )
 const style = StyleSheet.create({
@@ -32,4 +32,3 @@ const style = StyleSheet.create({
     alignItems: "center"
   }
 })
-
